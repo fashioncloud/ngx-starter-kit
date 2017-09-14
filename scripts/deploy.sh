3bc2602732f6aec6459dacf874fcb8a54d5145e8
Florian Klemt <florian.klemt@fashion.cloud>
2017-09-14 12:12:26 +0200

    Set aws region in deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,9 @@ npm run build
 DIR=dist
 BUCKET=ngx-starter-kit.fashion.cloud
 
+# configure aws cli
+aws configure set default.region eu-central-1
+aws configure set default.output json
+
 # sync app folder with s3 bucket
 aws  s3  sync $DIR s3://$BUCKET/ --delete
