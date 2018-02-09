0ad508fa2642aa32df90a5dc2e87438e070a5a62
Florian Klemt <florian.klemt@fashion.cloud>
2018-02-09 16:45:43 +0100

    Fixed tests

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,27 +1,21 @@
 import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        HomeComponent
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+  it('should create the component', async(() => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    const component = fixture.debugElement.componentInstance;
+    expect(component).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+  it(`should have as title 'Home'`, async(() => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    const component = fixture.debugElement.componentInstance;
+    expect(component.title).toEqual('Home');
   }));
 });
