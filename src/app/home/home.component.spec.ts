3e2be08addddbe26d0c601a6f6c6e08bbcc17b59
Florian Klemt <florian.klemt@fashion.cloud>
2018-02-28 14:16:20 +0100

    Fixed tests

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,18 +1,23 @@
 import { TestBed, async } from '@angular/core/testing';
+import { MatCardModule } from '@angular/material';
 import { HomeComponent } from './home.component';
-describe('AppComponent', () => {
+
+describe('HomeComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         HomeComponent
       ],
+      imports: [ MatCardModule ]
     }).compileComponents();
   }));
+
   it('should create the component', async(() => {
     const fixture = TestBed.createComponent(HomeComponent);
     const component = fixture.debugElement.componentInstance;
     expect(component).toBeTruthy();
   }));
+
   it(`should have as title 'Home'`, async(() => {
     const fixture = TestBed.createComponent(HomeComponent);
     const component = fixture.debugElement.componentInstance;
